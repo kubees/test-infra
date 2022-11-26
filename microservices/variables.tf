f443f9a170b6310e9c5933058bc6670da37debd8
AhmedGrati <ahmedgratti@think-it.io>
2022-11-26 18:42:31 +0100

    chore: add digest config

--- microservices/variables.tf
+++ microservices/variables.tf
@@ -2,3 +2,11 @@ variable "db_password" {
   type        = string
   description = "This is the redis database password."
 }
+
+variable "videos_microservice_image_digest" {
+  type = string
+}
+
+variable "playlist_microservice_image_digest" {
+  type = string
+}
