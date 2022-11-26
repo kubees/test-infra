d8a070cf908a16fb89807846060dfcb65f590487
AhmedGrati <48932084+AhmedGrati@users.noreply.github.com>
2022-11-26 18:54:01 +0100

    chore: integrate pre commit (#5)

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ module "redis" {
 }
 
 module "microservices" {
-  source      = "./microservices"
-  db_password = local.db_password
-  videos_microservice_image_digest = var.videos_microservice_image_digest
+  source                             = "./microservices"
+  db_password                        = local.db_password
+  videos_microservice_image_digest   = var.videos_microservice_image_digest
   playlist_microservice_image_digest = var.playlist_microservice_image_digest
   depends_on = [
     module.redis
@@ -22,4 +22,3 @@ module "frontend" {
     module.microservices
   ]
 }
-
